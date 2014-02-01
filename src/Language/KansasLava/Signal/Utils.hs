4d5b079d47a2c60a5b26c41bb0efff40fd339eb5
Dr. ERDI Gergo <gergo@erdi.hu>
2014-02-01 12:47:56 +0800

    add splitByte and debounce

--- src/Language/KansasLava/Signal/Utils.hs
+++ src/Language/KansasLava/Signal/Utils.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Language.KansasLava.Signal.Utils
-       ( nary
+       ( splitByte
+       , debounce
+       , nary
        , divideClk
        , counter
        , rotatorL
@@ -13,6 +15,35 @@ import Language.KansasLava
 import Data.Sized.Matrix as Matrix
 import Data.Sized.Unsigned as Unsigned
 import Data.Bits
+
+splitByte :: (sig ~ Signal c) => sig (Unsigned X8) -> (sig (Unsigned X4), sig (Unsigned X4))
+splitByte sig = (hi, lo)
+  where
+    mtx = fromUnsigned sig
+    hi = toUnsigned . flip cropAt 4 $ mtx
+    lo = toUnsigned . flip cropAt 0 $ mtx
+
+debounce :: forall c sig n. (Clock c, sig ~ Signal c, Size n)
+          => Witness n -> sig Bool -> (sig Bool, sig Bool, sig Bool)
+debounce _ button = runRTL $ do
+    -- Based on http://www.fpga4fun.com/Debouncer2.html
+    counter <- newReg (0 :: Unsigned n)
+    let counter_max = reg counter .==. maxBound
+    toggle <- newReg False
+
+    let idle = reg toggle ./=. button
+        down = bitNot (reg toggle) .&&. bitNot idle .&&. counter_max
+        up = reg toggle .&&. bitNot idle .&&. counter_max
+
+    CASE [ IF idle $ do
+                counter := 0
+         , OTHERWISE $ do
+                counter := reg counter + 1
+                WHEN counter_max $ do
+                    toggle := bitNot (reg toggle)
+         ]
+
+    return (up, down, reg toggle)
 
 nary :: forall a clk sig n. (Clock clk, sig ~ Signal clk, Rep a, Size n, Rep n) => sig n -> Matrix n (sig a) -> sig a
 nary sel inps = pack inps .!. sel
