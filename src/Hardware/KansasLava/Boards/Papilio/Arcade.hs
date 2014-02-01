8533619b6285f272a7093525459b2ab36aff0935
Dr. ERDI Gergo <gergo@erdi.hu>
2014-02-01 12:00:04 +0800

    Add support for the Arcade megawing

---
+++ src/Hardware/KansasLava/Boards/Papilio/Arcade.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE RecordWildCards #-}
+module Hardware.KansasLava.Boards.Papilio.Arcade (
+    -- * Class for the methods of the Spartan3e
+    Arcade(..)
+    -- * Initialization, and global settings.
+    , clockRate
+    , board_init
+    , writeUCF
+      -- * Data structures
+    , Buttons(..)
+    , VGA(..)
+    ) where
+
+import Language.KansasLava as KL
+import Hardware.KansasLava.VGA
+import Hardware.KansasLava.Boards.Papilio
+import Hardware.KansasLava.Boards.Papilio.UCF
+
+import Data.Sized.Ix
+import Data.Sized.Matrix
+import Control.Applicative
+import Control.Monad (ap, liftM)
+
+data Buttons = Buttons{ buttonUp, buttonDown
+                      , buttonLeft, buttonRight :: Seq Bool
+                      }
+
+class Papilio fabric => Arcade fabric where
+   resetButton :: fabric (Seq Bool)
+   buttons :: fabric Buttons
+   leds :: Matrix X4 (Seq Bool) -> fabric ()
+   vga :: VGA CLK X4 X4 X4 -> fabric ()
+
+writeUCF :: FilePath -> KLEG -> IO ()
+writeUCF = copyUCF "Arcade.ucf"
+
+instance Arcade Fabric where
+  resetButton = inStdLogic "BTN_RESET"
+
+  buttons = Buttons
+            `liftM` inStdLogic "BTN_UP"
+            `ap`    inStdLogic "BTN_DOWN"
+            `ap`    inStdLogic "BTN_LEFT"
+            `ap`    inStdLogic "BTN_RIGHT"
+
+  leds inp = outStdLogicVector "LED" (pack inp :: Seq (Matrix X4 Bool))
+
+  vga VGA{..} = do
+      outStdLogicVector "VGA_R" (pack vgaR :: Seq (Matrix X4 Bool))
+      outStdLogicVector "VGA_G" (pack vgaG :: Seq (Matrix X4 Bool))
+      outStdLogicVector "VGA_B" (pack vgaB :: Seq (Matrix X4 Bool))
+      outStdLogic "VGA_VSYNC" vgaVSync
+      outStdLogic "VGA_HSYNC" vgaHSync
